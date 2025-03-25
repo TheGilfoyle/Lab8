805e1e1710dff69cfeda8bd2a7897ea6803ca433
TheGilfoyle <budabelhassen@gmail.com>
2025-03-25 11:44:01 +0300

    On the way

--- src/main/java/org/example/commands/ExecuteScript.java
+++ src/main/java/org/example/commands/ExecuteScript.java
@@ -3,10 +3,11 @@ package org.example.commands;
 import org.example.Main;
 import org.example.exceptions.IncorrectArgsNumber;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
+import java.util.Scanner;
+
+import static org.example.Main.inv;
 
 /**
  * Команда для выполнения скрипта из файла.
@@ -24,49 +25,55 @@ public class ExecuteScript extends Command {
      */
     @Override
     public void execute() {
-//        try {
-//            String add = "src/main/resources/";//TODO
-//            String file = Main.console.getToken(1);
-//            String fileName = add + file;
-//
-//            try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-//                String line;
-//                while ((line = reader.readLine()) != null) {
-//                    line = line.trim();
-//                    if (line.isEmpty()) {
-//                        continue;
-//                    }
-//
-//                    System.out.println("Выполнение команды: " + line);
-//
-//                    String[] tokens = Arrays.stream(line.split(" "))
-//                            .filter(s -> !s.isEmpty())
-//                            .toArray(String[]::new);
-//
-//                    Main.console.setTokens(tokens);
-//
-//                    Command command = Main.inv.commands.get(tokens[0]);
-//                    if (command == null) {
-//                        System.out.println("Команда неизвестная: " + tokens[0]);
-//                        continue;
-//                    }
-//
-//                    if (tokens.length - 1 != command.getArgsAmount()) {
-//                        System.out.println("Неверное количество аргументов для команды: " + tokens[0]);
-//                        continue;
-//                    }
-//
-//                    try {
-//                        command.execute();
-//                    } catch (IncorrectArgsNumber e) {
-//                        System.out.println("Ошибка при выполнении команды: " + e.getMessage());
-//                    }
-//                }
-//            } catch (IOException e) {
-//                System.out.println("Ошибка при чтении файла: " + e.getMessage());
-//            }
-//        } catch (IndexOutOfBoundsException e) {
-//            System.out.println("Не указано имя файла для выполнения скрипта.");
-//        }
+        try {
+            String add = "src/main/resources/";//TODO
+            String file = Main.console.getToken(1);
+            String fileName = add + file;
+            InputStream previousInput = System.in;
+            try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+                System.setIn(new FileInputStream(fileName));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    line = line.trim();
+                    if (line.isEmpty()) {
+                        continue;
+                    }
+                    if (inv.getCommands().containsKey(line)) {
+                        System.out.println("Выполнение команды: " + line);
+                    } else {
+                        continue;
+                    }
+
+                    String[] tokens = Arrays.stream(line.split(" "))
+                            .filter(s -> !s.isEmpty())
+                            .toArray(String[]::new);
+
+                    Main.console.setTokens(tokens);
+
+                    Command command = inv.commands.get(tokens[0]);
+                    if (command == null) {
+                        System.out.println("Команда неизвестная: " + tokens[0]);
+                        continue;
+                    }
+
+                    if (tokens.length - 1 != command.getArgsAmount()) {
+                        System.out.println("Неверное количество аргументов для команды: " + tokens[0]);
+                        continue;
+                    }
+
+                    try {
+                        command.execute();
+                    } catch (IncorrectArgsNumber e) {
+                        System.out.println("Ошибка при выполнении команды: " + e.getMessage());
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Ошибка при чтении файла: " + e.getMessage());
+            }finally {
+                System.setIn(previousInput);
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Не указано имя файла для выполнения скрипта.");
+        }
     }
 }
